a081f95f153b61ed2af04a66118e61ec17b713e5
arijit <sparkler988323@gmail.com>
2020-02-21 00:41:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# mpu6050 with Raspberry Pi
+
+Accessing MPU-6050 on Raspberry Pi using Python to get temperature, accelerometer and gyroscope data.
+
+## Dependencies
+
+```
+sudo apt install python3-smbus
+
+pip install mpu6050-raspberrypi
+```
+
+## Run 
+
+```
+Python3 mpu_6050.py
+```
+
+
+## Authors
+
+**Arijit Das** 
+
+
+## Acknowledgments
+
+*The library used in this code is written by https://github.com/m-rtijn/mpu6050/
+* https://pypi.org/project/mpu6050-raspberrypi/
+
+
+
---
+++ mpu_6050.py
@@ -0,0 +1,22 @@
+from mpu6050 import mpu6050
+import time
+mpu = mpu6050(0x68)
+
+while True:
+    print("Temp : "+str(mpu.get_temp()))
+    print()
+
+    accel_data = mpu.get_accel_data()
+    print("Acc X : "+str(accel_data['x']))
+    print("Acc Y : "+str(accel_data['y']))
+    print("Acc Z : "+str(accel_data['z']))
+    print()
+
+    gyro_data = mpu.get_gyro_data()
+    print("Gyro X : "+str(gyro_data['x']))
+    print("Gyro Y : "+str(gyro_data['y']))
+    print("Gyro Z : "+str(gyro_data['z']))
+    print()
+    print("-------------------------------")
+    time.sleep(1)
+
